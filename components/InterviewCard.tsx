4c6e868e2eaec185105cc5ac2e35a35126fc3ffd
Prashant-bhadauriya <prashantbhadauriya033@gmail.com>
2025-04-16 00:08:33 +0530

    view interview implementaion in progress

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -3,10 +3,10 @@ import dayjs from 'dayjs'
 import Image from 'next/image';
 import { getRandomInterviewCover } from '@/lib/utils';
 import { Button } from './ui/button';
-import Link from 'next/link';
+import Link from 'next/link'; 
 import DisplayTechIcons from './DisplayTechIcons';
 
-const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+const InterviewCard = ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
     const feedback = null as Feedback | null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM DD, YYYY');
@@ -42,8 +42,9 @@ const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }
                 <DisplayTechIcons techStack={techstack} />
 
                 <Button className="btn-primary">
-                    <Link href={feedback ? `/interview/${interviewId}/feedback` : `/interview/${interviewId}`} />
+                    <Link href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`} >
                     {feedback ? 'Check Feedback' : 'View Interview'}
+                    </Link>
                 </Button>
             </div>
         </div>
