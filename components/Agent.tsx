de929911675d5597ae30643bb49cd33c19199f88
Prashant-bhadauriya <prashantbhadauriya033@gmail.com>
2025-04-17 00:29:19 +0530

    feedback page integration in process

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { interviewer } from "@/constants";
+import { createFeedback } from "@/lib/actions/general.action";
 import { cn } from "@/lib/utils";
 import { vapi } from "@/lib/vapi.sdk";
 import Image from "next/image";
@@ -60,12 +61,13 @@ const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) =
   }, []);
 
   const handleGeneralFeedback = async(messages: SavedMessage[]) => {
-       console.log('Generate feedbackhere.');
+       console.log('Generate feedback here.');
 
-       const { success, id } = {
-          success: true,
-          id: 'feedback-id'
-       }
+       const { success, feedbackId: id } = await createFeedback({
+          interviewId: interviewId!,
+          userId: userId!,
+          transcript: messages,
+       })
 
        if(success && id) {
           router.push(`/interview/$(interviewId)/feedback`);
